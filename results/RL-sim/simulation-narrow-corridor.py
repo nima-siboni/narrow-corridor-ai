9f4f516004e0ac2fcef497f540fa28f0d1c246dc
Nima Siboni <nima.siboni@gmail.com>
2020-04-21 12:55:29 +0200

    Mr La Linea!
    [modified] simulation-narrow-corridor.py : instead of the circles, la linea is used.
    
    [modified] results/RL-sim : modified by la linea

--- results/RL-sim/simulation-narrow-corridor.py
+++ results/RL-sim/simulation-narrow-corridor.py
@@ -1,76 +1,19 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+
 from RL_library import return_pointwise_A
 np.random.seed(0)
 
-
-def one_step(i, j, pi, sim):
-    A = return_pointwise_A(i, j, sim)
-    chosen_action_id = pi[i, j]
-    action = A[chosen_action_id]
-    iprime, jprime = np.array([i, j]) + action
-
-    # checking for the overalps
-    if iprime == jprime : # if the action is allowed; this part can be modified 
-        overlap = True
-    else:
-        overlap = False
-
-    # checking for the swaps
-    swap = False #swap is anyways always false when particles can move simultaneously, i.e. when sim == True
-    if (i == j - 1 and jprime == iprime - 1):
-        swap = True
-    if (i == j + 1 and jprime == iprime + 1):
-        swap = True
-
-    if (i == 2 and j == 9 and iprime == 9 and jprime == 2):
-        swap = True
-    if (i == 9 and j == 2 and iprime == 2 and jprime == 9):
-        swap = True
-
-    if (swap == True or overlap == True):
-        action_allowed = False
-        iprime = i
-        jprime = j
-    else:
-        action_allowed = True
-        
-    return iprime, jprime
-
-sim = True # True is simultaneous motion of particles are allowed 
-policyfilename = 'optimal_pi.dat'
-
-# reading the policy
-pi = np.loadtxt(policyfilename)
-pi = np.array(pi, dtype = int)
-n, n = np.shape(pi)
-
-
-# simulation loop
-i = 0
-j = 8
-plt.close('all')
-plt.ion()
-for x in range(0,9):
-    plt.scatter(x, 0, s=750, c='white', marker='s',linewidths=1, edgecolor='black' )
-plt.scatter(2, 1, s=750, c='white', marker='s',linewidths=1, edgecolor='black' )
-plt.scatter(i, 0, s=100, c='red')
-plt.scatter(j, 0, s=100, c='blue')
-plt.axis([-2, n+1, -2, 3])
-plt.axes().set_aspect('equal')
-plt.scatter(i, j, c='red')
-step = 0
-plt.savefig('state_000.png')
-while (i!=8 or j!=0):
-    step += 1
-    iy = jy = iyprime = jyprime = 0
-    iprime, jprime = one_step(i, j, pi, sim)
-    if (j == 0):
-        jprime = 0
-    if (i == 8):
-        iprime = 8
-
+##### plot_two_agents #####
+# plots two agents at i, j
+# with s = size
+# and color 1 and color 2
+def plot_two_agents(i, j, zoom, filename1, filename2):
+    # ix, and iy are calculated by converting the point i into and x, y pair
+    iy = jy = 0 
     ix = i
     jx = j
     if (i == 9):
@@ -79,23 +22,94 @@ while (i!=8 or j!=0):
     if (j == 9):
         jx = 2
         jy = 1
-    plt.scatter(ix, iy, s=100, c='#C1C7C9')
-    plt.scatter(jx, jy, s=100, c='#C1C7C9')
+    arr_lena = mpimg.imread(filename1)
+    imagebox = OffsetImage(arr_lena, zoom=zoom)
+    ab = AnnotationBbox(imagebox, (ix, iy), frameon=False)
+    ax = plt.axes()
+    ax.add_artist(ab)
+    arr_lena = mpimg.imread(filename2)
+    imagebox = OffsetImage(arr_lena, zoom=zoom)
+    ab = AnnotationBbox(imagebox, (jx, jy), frameon=False)
+    ax = plt.axes()
+    ax.add_artist(ab)
 
-    ixprime = iprime
-    jxprime = jprime
-    if (iprime == 9):
-        ixprime = 2
-        iyprime = 1
-    if (jprime == 9):
-        jxprime = 2
-        jyprime = 1
-    plt.axis([-2, n+1, -2, 3])
-    plt.scatter(ixprime, iyprime, s=100, c='red')
-    plt.scatter(jxprime, jyprime, s=100, c='blue')
+##### one_step #####
+# one action for agents at state i and j following
+# policy pi
+# given the allowance for sim(ultaneous) moves
+def one_step(i, j, pi, sim):
+    A = return_pointwise_A(i, j, sim)
+    chosen_action_id = pi[i, j]
+    action = A[chosen_action_id]
+    iprime, jprime = np.array([i, j]) + action
+    return iprime, jprime
 
-    
+
+#### The main program #####
+
+#### 1. initialization #####
+sim = True # True is simultaneous motion of particles are allowed 
+policyfilename = 'optimal_pi.dat'
+
+# 1.1 reading the policy
+pi = np.loadtxt(policyfilename)
+pi = np.array(pi, dtype = int)
+n, n = np.shape(pi)
+
+# 1.2 initial position of the agents
+i = 0
+j = 8
+
+
+# 1.3 plotting the grid 
+plt.close('all')
+plt.ion()
+ax = plt.axes()
+ax.axis('off')
+figure = plt.gcf()
+figure.set_size_inches(13, 5)
+boxsize = 2700
+for x in range(1,8):
+    plt.scatter(x, 0, s=boxsize, c='white', marker='s',linewidths=1, edgecolor='black' )
+plt.scatter(0, 0, s=boxsize, c='white', marker='s',linewidths=2, edgecolor='red' )
+plt.scatter(8, 0, s=boxsize, c='white', marker='s',linewidths=2, edgecolor='blue' )
+plt.scatter(2, 1, s=boxsize, c='white', marker='s',linewidths=1, edgecolor='black' )
+#plt.scatter(i, 0, s=100, c='red')
+#plt.scatter(j, 0, s=100, c='blue')
+plt.axis([-2, n+1, -2, 3])
+plt.axes().set_aspect('equal')
+plt.scatter(i, j, c='red')
+
+la_linea_left = './la_linea/la_linea_red_walking_left.png'
+la_linea_right = './la_linea/la_linea_blue_walking_right.png'
+zoom = 0.11
+#### 2. simulation  ####
+
+step = 0
+#plt.savefig('state_000.png')
+while (i!=8 or j!=0): # while the agents haven't arrived at their desired positions
+    # 2.1 one step following the policy
+    iprime, jprime = one_step(i, j, pi, sim)
+
+    if (jprime == 0): 
+        la_linea_left = './la_linea/la_linea_happy_red.png'
+    if (iprime == 8):
+        la_linea_right = './la_linea/la_linea_happy_blue.png'
+
+    # 2.2 checking if they have arrived
+    #if they arrive they dont leave anymore; not a major intervation
+    if (j == 0): 
+        jprime = 0
+        la_linea_left = './la_linea/la_linea_happy_red.png'
+    if (i == 8):
+        iprime = 8
+        la_linea_right = './la_linea/la_linea_happy_blue.png'
+    # 2.3 plotting
+    # plot_two_agents(i, j, 100, '#C1C7C9', '#C1C7C9') # plotting gray traces of the agents
+    plot_two_agents(i, j, zoom*1.05, './la_linea/white.png', './la_linea/white.png') # 
+    plot_two_agents(iprime, jprime, zoom, la_linea_right, la_linea_left) # plotting the agents in their new states
     plt.pause(0.1)
+    # 2.4 updating the position of the agents
     i = iprime
     j = jprime
     if (step < 10):
@@ -105,3 +119,4 @@ while (i!=8 or j!=0):
     elif (step < 1000):
         filename = 'state_'+str(step)+'.png'
     plt.savefig(filename)
+    step += 1
